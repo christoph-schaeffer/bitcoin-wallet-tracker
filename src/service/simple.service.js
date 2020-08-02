58fb914ff9decaf4c93bd0416112cfe50b836113
Christoph Dietrich <christoph-dietrich@web.de>
2020-08-02 17:03:21 +0200

    extended simple.service.js by some additional functions

--- src/service/simple.service.js
+++ src/service/simple.service.js
@@ -4,10 +4,21 @@ const resource = 'q';
 
 export default {
   get: type => ApiService.get(resource, type),
-  getMarketCap: () => ApiService.get(resource, 'marketcap'),
-  getTotalBc: () => ApiService.get(resource, 'totalbc'),
-  get24HrTransactionCount: () => ApiService.get(resource, '24hrtransactioncount'),
-  get24HrBtcSent: () => ApiService.get(resource, '24hrbtcsent'),
-  getHashrate: () => ApiService.get(resource, 'hashrate'),
-  getDifficulty: () => ApiService.get(resource, 'getdifficulty'),
+
+  getDifficulty: () => this.get(resource, 'getdifficulty'),
+  getBlockCount: () => this.get(resource, 'getblockcount'),
+  getBcPerBlock: () => this.get(resource, 'bcperblock'),
+  getTotalBc: () => this.get(resource, 'totalbc'),
+  getProbability: () => this.get(resource, 'probability'),
+  getInterval: () => this.get(resource, 'interval'),
+
+  getAddressBalance: (address) => this.ApiService(resource, `addressbalance/${address}`),
+  getAddressFirstSeen: (address) => this.ApiService(resource, `addressfirstseen/${address}`),
+
+
+  get24HrPrice: () => this.get(resource, '24hrprice'),
+  getMarketCap: () => this.get(resource, 'marketcap'),
+  get24HrTransactionCount: () => this.get(resource, '24hrtransactioncount'),
+  get24HrBtcSent: () => this.get(resource, '24hrbtcsent'),
+  getHashrate: () => this.get(resource, 'hashrate'),
 };
