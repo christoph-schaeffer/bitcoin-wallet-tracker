18708f46182f22183b49a210e3e3af98dfe9c6f5
Christoph Dietrich <christoph-dietrich@web.de>
2020-08-04 12:30:59 +0200

    implemented unstyled myBitcoins.vue

--- src/views/MyBitcoins.vue
+++ src/views/MyBitcoins.vue
@@ -1,9 +1,34 @@
 <template lang="pug">
-  div henlo this is MyBitcoins such amaze
+  v-card
+    v-text-field(v-model="addressField", :label="$t('myBitcoins.address')" )
+    v-btn(@click="fetchBalance(addressField)") {{$t('myBitcoins.addAddress')}}
+    v-row
+      v-col(cols="12" v-for="balance in balances" :key="balance.address")
+        p address: {{balance.address}}
+        p btc: {{balance.btc | localizedNumber(0,8)}}
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'MyBitcoins',
+  computed: {
+    ...mapGetters('balance', [
+      'balances',
+    ]),
+  },
+  methods: {
+    ...mapActions('balance', [
+      'fetchBalance',
+      'updateBalance',
+      'removeBalance',
+    ]),
+  },
+  data() {
+    return {
+      addressField: null,
+    };
+  },
 };
 </script>
