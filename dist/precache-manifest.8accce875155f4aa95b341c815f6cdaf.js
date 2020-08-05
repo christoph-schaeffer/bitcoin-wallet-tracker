908f70c43f8c5a74e776d68ecc9de57365ff8b79
Christoph Dietrich <christoph-dietrich@web.de>
2020-08-05 10:46:22 +0200

    build production code

---
+++ dist/precache-manifest.8accce875155f4aa95b341c815f6cdaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e951be0a4535b22de2",
+    "url": "/bitcoin-wallet-tracker/demo/css/app.a1f00e28.css"
+  },
+  {
+    "revision": "91bed40313a7585eccef",
+    "url": "/bitcoin-wallet-tracker/demo/css/chunk-vendors.23467814.css"
+  },
+  {
+    "revision": "f1b67d437c503c5e4a9f0da7584a0bf6",
+    "url": "/bitcoin-wallet-tracker/demo/img/drawerBackground.f1b67d43.jpg"
+  },
+  {
+    "revision": "37edfcdefbbb8d303ffc3ca6289a988f",
+    "url": "/bitcoin-wallet-tracker/demo/img/logo.37edfcde.svg"
+  },
+  {
+    "revision": "de4bee7b7d973d2bd07b52e3771ef120",
+    "url": "/bitcoin-wallet-tracker/demo/index.html"
+  },
+  {
+    "revision": "10e951be0a4535b22de2",
+    "url": "/bitcoin-wallet-tracker/demo/js/app.be79e261.js"
+  },
+  {
+    "revision": "91bed40313a7585eccef",
+    "url": "/bitcoin-wallet-tracker/demo/js/chunk-vendors.83b9df57.js"
+  },
+  {
+    "revision": "7037c2cfc132060f9c8a9779798a9d63",
+    "url": "/bitcoin-wallet-tracker/demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bitcoin-wallet-tracker/demo/robots.txt"
+  }
+]);
